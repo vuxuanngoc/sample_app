e27de113b5c80f3dab945cf2d690e2c2bfe455d7
ngocvu <ngocvu811995@gmail.com>
2019-07-15 17:48:13 +0700

    fix file production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     port: 587,
     user_name: 'ngocsociu811995@gmail.com',
     password: 'ngocsociu',
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true
   }
 
